4438f6e68c839c3b85d8dade9d8cb99cc40f0655
99xtal <joerybarczyk99@gmail.com>
2026-06-29 23:43:17 -0400

    imgui setup

--- debugger/src/main.cpp
+++ debugger/src/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <stdio.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 void glfw_error_callback(int error, const char* description) {
-    (void)error;
-    std::cerr << std::format("GLFW Error: {}\n", description);
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
 int main() {
+    // glfw setup
     glfwSetErrorCallback(glfw_error_callback);
 
     if (!glfwInit()) {
@@ -17,7 +22,9 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(256, 224, "nex", NULL, NULL);
+
+    float main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor());
+    GLFWwindow* window = glfwCreateWindow((int)(256 * 3 * main_scale), (int)(224 * 3 * main_scale), "nex", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -29,13 +36,77 @@ int main() {
     }
     glfwSwapInterval(1);
 
-    
-    while (!glfwWindowShouldClose(window)) {
-        // render loop
+    // imgui setup
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
 
-        glfwSwapBuffers(window);
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    while (!glfwWindowShouldClose(window)) {
+        // Poll events
         glfwPollEvents();
+
+        // setup window
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (ImGui::BeginMainMenuBar()) {
+            if (ImGui::BeginMenu("File")) {
+                if (ImGui::MenuItem("Open ROM...")) {
+                    // ...
+                }
+
+                if (ImGui::MenuItem("Quit")) {
+                    glfwSetWindowShouldClose(window, GLFW_TRUE);
+                }
+
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("View")) {
+                ImGui::MenuItem("CPU", nullptr, nullptr);
+                ImGui::MenuItem("PPU", nullptr, nullptr);
+                ImGui::MenuItem("Memory", nullptr, nullptr);
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Emulation")) {
+                ImGui::MenuItem("Pause", nullptr, nullptr);
+
+                if (ImGui::MenuItem("Reset")) {
+                    // nex_reset(nes);
+                }
+
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMainMenuBar();
+        }
+
+        // rendering
+
+        ImGui::Render();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     
     glfwDestroyWindow(window);
     glfwTerminate();
