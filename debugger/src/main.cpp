c583e20bd59fbcca237895484781bbf4ccaddb46
99xtal <joerybarczyk99@gmail.com>
2026-06-29 23:09:14 -0400

    GLFW/GLAD setup

--- debugger/src/main.cpp
+++ debugger/src/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void glfw_error_callback(int error, const char* description) {
+    (void)error;
+    std::cerr << std::format("GLFW Error: {}\n", description);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwSetErrorCallback(glfw_error_callback);
+
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(256, 224, "nex", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL()) {
+        std::cerr << "GLAD Error: Failed to initialize GLAD\n";
+    }
+    glfwSwapInterval(1);
+
+    
+    while (!glfwWindowShouldClose(window)) {
+        // render loop
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
